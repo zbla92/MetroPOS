152ddb374fa5535238c616b58bc6feb7b817a3c7
Zbla92 <milanblaz2580456@gmail.com>
2019-05-03 02:51:41 +0200

    updating Register component Styles

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,45 +1,47 @@
-import React from 'react';
+import React from "react";
 
-class Clock extends React.Component{
-    constructor(){
-        super()
-        
-        let d = new Date();
-        this.state = {
-            intervalID: 0,
-            h: d.getHours(),
-            m: d.getMinutes(),
-            s: d.getSeconds()
-        }
-    this.countingSeconds = this.countingSeconds.bind(this)
-    }
+class Clock extends React.Component {
+  constructor() {
+    super();
 
-    countingSeconds(){
-          let d = new Date()
-          this.setState({
-            h: d.getHours(),
-            m: d.getMinutes(),
-            s: d.getSeconds()
-          })  
-          console.log(this.state.s)
-    }
+    let d = new Date();
+    this.state = {
+      intervalID: 0,
+      h: d.getHours(),
+      m: d.getMinutes(),
+      s: d.getSeconds(),
+    };
+    this.countingSeconds = this.countingSeconds.bind(this);
+  }
 
-    renderTime(e){
-        return e > 9 ? e : `0` + e; 
-    }
+  countingSeconds() {
+    let d = new Date();
+    this.setState({
+      h: d.getHours(),
+      m: d.getMinutes(),
+      s: d.getSeconds(),
+    });
+    console.log(this.state.s);
+  }
 
-    componentWillMount(){
-             this.setState({intervalID: setInterval(this.countingSeconds, 1000)})
-    }
-    componentWillUnmount(){
-            clearInterval(this.state.intervalID)
-    }
+  renderTime(e) {
+    return e > 9 ? e : `0` + e;
+  }
 
-    render(){
-        return(
-            <div>{this.renderTime(this.state.h)}:{this.renderTime(this.state.m)}:{this.renderTime(this.state.s)}</div>
-        );        
-        }
+  componentWillMount() {
+    this.setState({ intervalID: setInterval(this.countingSeconds, 1000) });
+  }
+  componentWillUnmount() {
+    clearInterval(this.state.intervalID);
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderTime(this.state.h)}:{this.renderTime(this.state.m)}
+      </div>
+    );
+  }
 }
 
 export default Clock;
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,29 +1,79 @@
-import itemList from '../data/items/items.json'
-import '../css/register.css'
+import itemList from '../data/items/items.json';
+import '../css/register.css';
 import React from 'react';
 import Clock from './Clock';
 
-class Register extends React.Component{
-    constructor(props){
-        super(props)
+class Register extends React.Component {
+    constructor(props) {
+        super(props);
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <div className="ui grid">
                     <div className="sixteen wide column info-bar">
                         <div className="ui  three column grid">
-                            <div className="column"></div>
-                            <div className="column clock"><Clock /></div>
-                            <div className="column"></div>
+                            <div className="column" />
+                            <div className="column clock">
+                                <Clock />
+                            </div>
+                            <div className="column" />
                         </div>
                     </div>
-                    <div className="ten wide column"></div>
-                    <div className="six wide column"></div>
+                    <div className="sixteen wide column info-panel">logout</div>
+                </div>
+                <div className="ui grid">
+                    <div className="six wide column display-container">
+                        <div className="ui grid">
+                            <table className="table-top-buttons">
+                                <tr>
+                                    <th>
+                                        <button className="pos-btn">
+                                            To go
+                                        </button>
+                                    </th>
+                                    <th>
+                                        <button className="pos-btn">Tab</button>
+                                    </th>
+                                    <th>
+                                        <button className="pos-btn">
+                                            Delivery
+                                        </button>
+                                    </th>
+                                    <th>
+                                        <button className="pos-btn">
+                                            Number
+                                        </button>
+                                    </th>
+                                </tr>
+                            </table>
+                        </div>
+                        <div className="items-container">item</div>
+                        <div className="totals-container">totals</div>
+                        <table className="table-bottom-buttons">
+                            <tr>
+                                <th>
+                                    <button className="pos-btn">To go</button>
+                                </th>
+                                <th>
+                                    <button className="pos-btn">Tab</button>
+                                </th>
+                                <th>
+                                    <button className="pos-btn">
+                                        Delivery
+                                    </button>
+                                </th>
+                                <th>
+                                    <button className="pos-btn">Number</button>
+                                </th>
+                            </tr>
+                        </table>
+                    </div>
+                    <div className="ten wide column menus-container">s</div>
                 </div>
             </div>
-        )
+        );
     }
 }
-export default Register; 
+export default Register;
