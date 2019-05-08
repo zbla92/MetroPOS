6f09e24159cb5127bd2922d02a97f42e709c575c
Zbla92 <milanblaz2580456@gmail.com>
2019-05-08 10:10:04 +0200

    updated menu , fixed menu buttons / colors are weird

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -76,7 +76,37 @@ class Register extends React.Component {
             </div>
           </div>
           <div className='main-right-container'>
-            <LoadSubmenu />
+            <div className="submenu-injection-container">
+              <LoadSubmenu />
+              <LoadSubmenu />
+              <LoadSubmenu />
+              <LoadSubmenu />
+              <LoadSubmenu />
+            </div>
+            <div className="submenu-selection-container">
+              <div className="button-row">
+                  <button id="drinks-btn" className="btn-top">Drinks</button>
+                  <button id="apetizers-btn" className="btn-top">Apetizers</button>
+                  <button id="salads-btn" className="btn-top">Salads</button>
+                  <button id="sandwitch-btn" className="btn-top">Sandwitches</button>
+                  <button id="burgers-btn" className="btn-top">Burgers</button>
+                  <button id="pizzas-btn" className="btn-top">Pizza</button>
+                  <button id="soup-btn"className="btn-top">Soups</button>
+                  <button id="kids-menu-btn" className="btn-top">Kids menu</button>
+                  <button id="cake-btn" className="btn-top">Cake</button>
+              </div>
+              <div className="button-row">
+                  <button id="flat-bread-btn" className="btn-top">Flat Breads</button>
+                  <button id="dinner-plates-btn" className="btn-top">Dinner Plates</button>
+                  <button id="dessert-btn" className="btn-top">Dessert</button>
+                  <button id="vegetarian-btn" className="btn-top">Vegetarian</button>
+                  <button id="pasta-btn" className="btn-top">Pasta</button>
+                  <button id="alchocol-btn" className="btn-top">Alcohol</button>
+                  <button id="chicken-btn" className="btn-top">Chicken</button>
+                  <button id="steak-btn" className="btn-top">Steak</button>
+                  <button id="palstic-btn" className="btn-top">Plastic</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
