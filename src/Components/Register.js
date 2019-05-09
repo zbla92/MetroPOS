cf9c20867ceffe584541df086a03f9561f06cf04
Blaz <mb250653@ncr.com>
2019-05-09 11:50:13 -0400

    commiting

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -6,6 +6,7 @@ import Clock from './Clock';
 import Item from './Item';
 import TotalPrice from './TotalPrice';
 import LoadSubmenu from './LoadSubmenu';
+import MenuButtons from './MenuButtons';
 
 class Register extends React.Component {
     constructor(props) {
@@ -108,67 +109,7 @@ class Register extends React.Component {
                             <LoadSubmenu />
                         </div>
                         <div className="submenu-selection-container">
-                            <div className="button-row">
-                                <button id="drinks-btn" className="btn-top">
-                                    Drinks
-                                </button>
-                                <button id="apetizers-btn" className="btn-top">
-                                    Apetizers
-                                </button>
-                                <button id="salads-btn" className="btn-top">
-                                    Salads
-                                </button>
-                                <button id="sandwitch-btn" className="btn-top">
-                                    Sandwitches
-                                </button>
-                                <button id="burgers-btn" className="btn-top">
-                                    Burgers
-                                </button>
-                                <button id="pizzas-btn" className="btn-top">
-                                    Pizza
-                                </button>
-                                <button id="soup-btn" className="btn-top">
-                                    Soups
-                                </button>
-                                <button id="kids-menu-btn" className="btn-top">
-                                    Kids menu
-                                </button>
-                                <button id="cake-btn" className="btn-top">
-                                    Cake
-                                </button>
-                            </div>
-                            <div className="button-row">
-                                <button id="flat-bread-btn" className="btn-top">
-                                    Flat Breads
-                                </button>
-                                <button
-                                    id="dinner-plates-btn"
-                                    className="btn-top"
-                                >
-                                    Dinner Plates
-                                </button>
-                                <button id="dessert-btn" className="btn-top">
-                                    Dessert
-                                </button>
-                                <button id="vegetarian-btn" className="btn-top">
-                                    Vegetarian
-                                </button>
-                                <button id="pasta-btn" className="btn-top">
-                                    Pasta
-                                </button>
-                                <button id="alchocol-btn" className="btn-top">
-                                    Alcohol
-                                </button>
-                                <button id="chicken-btn" className="btn-top">
-                                    Chicken
-                                </button>
-                                <button id="steak-btn" className="btn-top">
-                                    Steak
-                                </button>
-                                <button id="palstic-btn" className="btn-top">
-                                    Plastic
-                                </button>
-                            </div>
+                            <MenuButtons />
                         </div>
                     </div>
                 </div>
